3acbc140fd9b45f4a5176421268b763b21e5da40
Munieru <20086673+munierujp@users.noreply.github.com>
2023-03-28 02:29:34 +0900

    Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
-// eslint-disable-next-line unicorn/prefer-module
-module.exports = {
+// @ts-check
+
+// eslint-disable-next-line jsdoc/valid-types -- https://www.typescriptlang.org/docs/handbook/jsdoc-supported-types.html
+/** @typedef {import('eslint').ESLint.ConfigData} ConfigData */
+
+/**
+ * @type {ConfigData}
+ * @see https://eslint.org/docs/latest/use/configure/configuration-files
+ */
+const config = {
   extends: [
     '@eslint-recommended/eslint-config-typescript',
     'plugin:jest/recommended'
@@ -8,3 +16,6 @@ module.exports = {
     project: './tsconfig.json'
   }
 }
+
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = config
